9de47f93567f12bec71b876c513d4dce44c37161
Taewoong Moon <moondingie5@gmail.com>
2019-09-23 16:55:56 +0900

    웹팩 초기 설정

---
+++ lecture/WordRelay.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+const { Component } = React;
+
+class WordRelay extends Component {
+    state = {
+        text: "hello world"
+    }
+
+    render() {
+        return (
+            <h1>{this.state.text}</h1>
+        )
+    }
+}
+
+module.exports = WordRelay;
