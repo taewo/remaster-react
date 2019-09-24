abe0ec663333b7543adc0260a1e3f936735b2001
Taewoong Moon <moondingie5@gmail.com>
2019-09-24 11:46:29 +0900

    #2-7 바벨설정 - 브라우져 타겟 설정

--- 1/webpack.config.js
+++ 1/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
     name: 'gugudan-setting',
@@ -16,11 +17,24 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties']
-            }
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {  //  @babel/preset-env의 브라우저 지원 설정. https://github.com/browserslist/browserslist
+                            browsers: ['> 1% in KR', 'last 2 chrome versions']
+                        },
+                        debug: true,    //  콘솔 디버깅
+                    }],
+                    '@babel/preset-react'
+                ],
+                plugins: [
+                    "@babel/plugin-proposal-class-properties"
+                ],
+            },
         }]
     },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+    ],
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js'
